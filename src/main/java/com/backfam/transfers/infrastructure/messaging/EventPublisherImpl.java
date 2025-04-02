890b1abf840408e841c05197173024cdac2e8782
BorisMejia <borisfernandez916@gmail.com>
2025-04-02 15:28:39 -0500

    se agrega conexion y configuracion de rabbitmq

--- src/main/java/com/backfam/transfers/infrastructure/messaging/EventPublisherImpl.java
+++ src/main/java/com/backfam/transfers/infrastructure/messaging/EventPublisherImpl.java
@@ -1,2 +1,21 @@
-package com.backfam.transfers.infrastructure.messaging;public class EventPublisherImpl {
+package com.backfam.transfers.infrastructure.messaging;
+
+import com.backfam.transfers.application.event.EventPublisher;
+import com.backfam.transfers.domain.event.TransactionCreateEvent;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventPublisherImpl implements EventPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public EventPublisherImpl(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void publicEvent(TransactionCreateEvent event){
+        rabbitTemplate.convertAndSend("bank.transaction", "transactionRoutingKey", event);
+    }
 }
