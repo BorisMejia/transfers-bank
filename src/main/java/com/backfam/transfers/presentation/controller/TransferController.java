e1cad8885f23b249a9bdccc9e5cb503c95794183
BorisMejia <borisfernandez916@gmail.com>
2025-04-29 23:52:08 -0500

    Actualiza las rutas de los controladores y configura el contexto del servidor en application.properties

--- src/main/java/com/backfam/transfers/presentation/controller/TransferController.java
+++ src/main/java/com/backfam/transfers/presentation/controller/TransferController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/transfer")
+@RequestMapping("/transfer")
 public class TransferController {
 
     private final TransferService transferService;
